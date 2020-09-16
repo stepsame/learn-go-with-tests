dab12c90db211688c78d866962881f96783560fb
butong <butong@xiaoniangao.com>
2020-09-16 22:38:19 +0800

    structs v3

--- structs/shapes.go
+++ structs/shapes.go
@@ -27,4 +27,13 @@ func Perimeter(rectangle Rectangle) float64 {
 	return 2 *(rectangle.Width + rectangle.Height)
 }
 
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * 0.5
+}
+
 
